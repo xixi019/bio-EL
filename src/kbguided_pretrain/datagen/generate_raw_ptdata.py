dee710d2b37aa13f4401ac71068fb9296ddc73f2
xixi019 <jenswe@163.com>
2023-04-24 14:38:16 +0200

    linebyline triple injection
    latest version of data generation code

--- src/kbguided_pretrain/datagen/generate_raw_ptdata.py
+++ src/kbguided_pretrain/datagen/generate_raw_ptdata.py
@@ -2,7 +2,7 @@ import os
 from tqdm import tqdm
 import re
 from random import shuffle
-import pickle
+import numpy
 import copy
 import sys
 import pandas as pd
@@ -59,6 +59,7 @@ class UMLS(object):
         '''
         self.cui_in_onto = set()
         self.cui2triple = defaultdict(set)
+        self.relStat = defaultdict(lambda:0)
         self.cui2pref = dict()
 
         for line in tqdm(rel_reader, ascii=True):
@@ -69,10 +70,14 @@ class UMLS(object):
             if len(l[7])>0:
                 cui = l[0]
                 rel = l[7]
+                self.relStat[rel] += 1
                 object = l[4]
                 self.cui2triple[cui].add((rel, object))
         # number of triples in total in the pre-training datax
         print("the length of cui in the umls dump is", len(self.cui2triple)) 
+        totalRel = sum(self.relStat.values())
+        for rel in self.relStat.keys():
+            self.relStat[rel] = self.relStat[rel]/totalRel
 
 
         '''
@@ -97,6 +102,7 @@ class UMLS(object):
                 else:
                     self.cui2pref[cui] = [string]
                 if source in source_onto:
+                    # which 
                     self.cui_in_onto.update([cui])
         self.cuis_in_semtc = {}
         for line in tqdm(semantic_reader, ascii=True):
@@ -196,39 +202,148 @@ def create_line(prefix, mention, context, special_tokens, template_choice):
 
 
 # create the synthetic text based on cui and triple it connects to
-rels = ["has_entry_version", "has_sort_version", "entry_version_of", "permuted_term_of", "sort_version_of"]
-def trip2txt(mention, triples, cui2syns):
+rels = ["has_entry_version", "has_sort_version", "entry_version_of", "permuted_term_of", "sort_version_of", "has_permuted_term", "mapped_to"]
+def trip2txt(mention, triples, cui2syns, mode, relStat):
     '''
     triples: triple this entity is connected to
     '''
+    n_sample = 5
     synText = []
     if len(triples)<=100:
         for pair in triples:
             if pair[1] in cui2syns and pair[0] not in rels:
-                synText.extend(pair[0].split('_'))
-                synText.append(random.choice(cui2syns[pair[1]]) + '.')
-            else:
-                pass
-        synText = " ".join(synText)
+                text = []
+#                text.extend(pair[0].split('_'))
+                text.extend([pair[0]])
+                text.append(random.choice(cui2syns[pair[1]]) + '.')
+                synText.append(text)
+        if len(synText) == 0:
+            return synText
+
+        if mode == 'Tri':
+            for idx in range(len(synText)):
+                synText[idx][0] = synText[idx][0].split('_')
+            synText = " ".join(synText)
+
+        if mode == 'ByLine':
+            if len(synText) <= n_sample:
+                for idx in range(len(synText)):
+                    rel = synText[idx][0].split('_')
+                    tail = synText[idx][-1]
+                    text = []
+                    text.extend(rel)
+                    text.append(tail)
+                    synText[idx] = text
+                synText = [ " ".join(i) for i in synText]
+                return synText
+
+            pairDir = defaultdict(list)
+            # create a dictionary for key ---> [tails]
+            for pair in synText:
+                rel, tail = pair[0], pair[1]
+                pairDir[rel].append(rel +'__:__'+ tail)
+            seq, probs = list(), list()
+            for rel in pairDir.keys():
+                tails = pairDir[rel]
+                seq.append(tails)
+                probs.append(relStat[rel] * n_sample) 
+            # sums up to 1
+            probs = [i/sum(probs) for i in probs]
+            seq = numpy.array(seq)
+            indices = numpy.arange(seq.shape[0])
+
+            output = seq[numpy.random.choice(a=indices, p=probs)]
+            output = [item for item in output]
+
+            while len(output) <  n_sample:
+                addi = seq[numpy.random.choice(a=indices, p=probs)]
+                addi = [item for item in output]
+                output.extend(addi)
+
+            output = output[:n_sample-1]
+
+            out= list()
+            for pair in output:
+                rel, tail = pair.split('__:__')
+                text = []
+                text.extend(rel.split('_'))
+                text.append(tail)
+                out.append( " ".join(text))
+            return out
+
     else:
         for pair in triples:
             if random.randint(0, len(triples)) <= 99:
                 if pair[1] in cui2syns and pair[0] not in rels:
-                    synText.extend(pair[0].split('_'))
-                    synText.append(random.choice(cui2syns[pair[1]]) + ".")
-                else:
-                    pass
+                    text = []
+                    text.extend([pair[0]])
+                    text.append(random.choice(cui2syns[pair[1]]) + '.')
+                    synText.append(text)
+        if len(synText) == 0:
+            return synText
 
-        synText = " ".join(synText)
+        if mode == 'Tri':
+            synText = " ".join(synText)
+
+        if mode == 'ByLine':
+            if len(synText) <= n_sample:
+                for idx in range(len(synText)):
+                    rel = synText[idx][0].split('_')
+                    tail = synText[idx][-1]
+                    text = []
+                    text.extend(rel)
+                    text.append(tail)
+                    synText[idx] = text
+                synText = [ " ".join(i) for i in synText]
+                return synText
+
+            pairDir = defaultdict(list)
+            # create a dictionary for key ---> [tails]
+            for pair in synText:
+                rel, tail = pair[0], pair[1]
+                pairDir[rel].append(rel +'__:__'+ tail)
+            seq, probs = list(), list()
+            for rel in pairDir.keys():
+                tails = pairDir[rel]
+                seq.append(tails)
+                probs.append(relStat[rel] * n_sample) 
+            # sums up to 1
+            probs = [i/sum(probs) for i in probs]
+            seq = numpy.array(seq)
+            indices = numpy.arange(seq.shape[0])
+
+            output = seq[numpy.random.choice(a=indices, p=probs)]
+            output = [item for item in output]
+
+            while len(output) <  n_sample:
+                addi = seq[numpy.random.choice(a=indices, p=probs)]
+                addi = [item for item in output]
+                output.extend(addi) 
+            output = output[:n_sample-1]
+
+            out= list()
+            for pair in output:
+                rel, tail = pair.split('__:__')
+                text = []
+                text.extend(rel.split('_'))
+                text.append(tail)
+                out.append( " ".join(text))
+            return out
+
     return synText
 
-def prepare_final_pretraindata(cui2defs, cui2syns, cui2triple, special_tokens = None, select_scheme = 'random', OnlySyn = False):
+def prepare_final_pretraindata(cui2defs, cui2syns, cui2triple, relStat, special_tokens = None, select_scheme = 'random', mode = 'ByLine'):
+    '''
+    mode = str (ByLine, Syn, Tri) inficating different mode of pre-training
+    '''
     from transformers import BartTokenizer
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
     output = []
     for cui in tqdm(cui2syns):
         triples = cui2triple[cui]
         for syn in cui2syns[cui]:
+            # generate description
+            #synthesize a description line if cui does not have a description saved in KB.
             if cui not in cui2defs:
                 if len(cui2syns[cui]) > 1:
                     pending_set = copy.deepcopy(cui2syns[cui])
@@ -260,16 +375,27 @@ def prepare_final_pretraindata(cui2defs, cui2syns, cui2triple, special_tokens = 
                         des = tokenizer.decode(tks[-512:])
 
             output.append([cui, mention, syn, des, None])
-            if OnlySyn:
+            if mode == 'Syn':
                 continue
         # syntext is the linearilized triples
             if len(triples) >= 1:
-                synText = trip2txt(mention, triples, cui2syns)
-                tks = tokenizer(synText)['input_ids']
-                if len(tks) > 512:
-                    synText = tokenizer.decode(tks[:512])
-                    # more than <s></s>
-                output.append([cui, mention, syn, des, synText])
+                synText = trip2txt(mention, triples, cui2syns, mode, relStat)
+                # in case there's no text generated
+                if len(synText) >= 1:
+                    if mode == 'Tri':
+                        tks = tokenizer(synText)['input_ids']
+                        if len(tks) > 512:
+                            synText = tokenizer.decode(tks[:512])
+                            # more than <s></s>
+                        output.append([cui, mention, syn, des, synText])
+                    if mode == 'ByLine':
+                        for line in synText:
+                            tks = tokenizer(line)['input_ids']
+                            if len(tks) > 512:
+                                line = tokenizer.decode(tks[:512])
+                                # more than <s></s>
+                            output.append([cui, mention, syn, des, line])
+
 
     random.shuffle(output)
     return output
@@ -281,6 +407,8 @@ if __name__ ==  '__main__':
 
     semantic_type = set(['T005','T007','T017','T022','T031','T033','T037','T038','T058','T062','T074',
                     'T082','T091','T092','T097','T098','T103','T168','T170','T201','T204'])
+    import os
+    print(os.getcwd())
     semantic_type_ontology = pd.read_csv('STY.csv') # TUI->STRING mapping table
     semantic_type_size = 0
     while len(semantic_type)!=semantic_type_size:
@@ -289,8 +417,8 @@ if __name__ ==  '__main__':
             if semantic_type_ontology['Parents'][i][-4:] in semantic_type:
                 semantic_type.update([semantic_type_ontology['Class ID'][i][-4:]])
     source_onto = ['CPT','FMA','GO','HGNC','HPO','ICD10','ICD10CM','ICD9CM','MDR','MSH','MTH',
-                    'NCBI','NCI','NDDF','NDFRT','OMIM','RXNORM','SNOMEDCT_US']
-    UMLS = UMLS('/export/home/moeller/bio-el/', only_load_dict = True)
+                    'NCBI','NCI','NDDF','NDFRT','OMIM','RXNORM','SNOMEDCT_US', "SNOMEDCT_CT"]
+    UMLS = UMLS('/export/home/yan/el/', only_load_dict = True)
 
     UMLS.generate_name_list_set(semantic_type, source_onto)
     UMLS.generate_syn_des()
@@ -303,14 +431,14 @@ if __name__ ==  '__main__':
             count += 1
     print(count)
 
-    output = prepare_final_pretraindata(UMLS.cui2description, UMLS.cui2pref, UMLS.cui2triple, special_tokens = ["START", "END"], select_scheme = 'random', OnlySyn = False)
+    output = prepare_final_pretraindata(UMLS.cui2description, UMLS.cui2pref, UMLS.cui2triple, UMLS.relStat, special_tokens = ["START", "END"], select_scheme = 'random', mode = 'ByLine')
     shuffle(output)
     f = None
-    if not os.path.exists('./raw_data/'):
-        os.makedirs('./raw_data/')
+    if not os.path.exists('./test0_raw_data/'):
+        os.makedirs('./test0_raw_data/')
     for i in tqdm(range(len(output))):
         if i%100000 == 0:
             if f:
                 f.close()
-            f = open('./raw_data/data_'+str(i//100000).rjust(3,'0')+'.txt', 'w')
+            f = open('./test0_raw_data/data_'+str(i//100000).rjust(3,'0')+'.txt', 'w')
         f.write(json.dumps(output[i])+'\n')
